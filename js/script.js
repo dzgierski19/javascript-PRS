afee33b843406cc06547a12c19d8f1c15f09a4ee
Dawid Zgierski <dzgierskigit@gmail.com>
2022-08-02 08:09:52 +0200

    game combinations

--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@ if(randomNumber == 1){
 }
 
 
-
 printMessage('Mój ruch to ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1:kamień, 2: papier, 3: nożyce.');
@@ -22,11 +21,35 @@ console.log('Gracz wpisał: ' + playerInput);
 let playerMove = 'nieznany ruch';
 
 if(playerInput == 1 ){
- playerMove = 'kamień';
+    playerMove = 'kamień';
 } else if (playerInput == 2){
- playerMove = 'papier';
+    playerMove = 'papier';
 } else if (playerInput == 3){
- playerMove = 'nożyce';
+    playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == playerMove){
+    printMessage('remis');
+} else if (computerMove == 'papier' && playerMove =='kamień'){
+    printMessage('Ty przegrywasz');
+} else if (computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ty przegrywasz');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ty przegrywasz');
+} else if (playerMove == 'nieznany ruch'){
+    printMessage('Wpisz inną liczbę');
+}
+
+
+
+
+
+
