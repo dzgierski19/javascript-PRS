e5845a0ad6800b890f3227c2bb2c29e4de41f06c
Dawid Zgierski <dzgierskigit@gmail.com>
2022-08-03 22:38:26 +0200

    game finished

--- js/script.js
+++ js/script.js
@@ -1,54 +1,75 @@
-function playGame() {
+{    function playGame(playerInput) {
 
-    let randomNumber = Math.floor(Math.random() *3 + 1)
+        clearMessages();
 
-    console.log('Wylosowana liczba to: ' + randomNumber)
+        let randomNumber = Math.floor(Math.random() *3 + 1)
 
-    function getMoveName(argMoveId){
-        if(argMoveId == 1){
-            return 'kamień';
-        }else if (argMoveId == 2){
-            return 'papier';
-        }else if (argMoveId == 3){
-            return 'nożyce';
-        }else {
-            printMessage ('Spróbuj jeszcze raz. Wpisz liczbę od 1 do 3');
+        console.log('Wylosowana liczba to: ' + randomNumber)
+
+        function getMoveName(argMoveId){
+            if(argMoveId == 1){
+                return 'kamień';
+            }else if (argMoveId == 2){
+                return 'papier';
+            }else if (argMoveId == 3){
+                return 'nożyce';
+            }else {
+                printMessage ('Spróbuj jeszcze raz. Wpisz liczbę od 1 do 3')
+                return 'Nie znam takiej cyfry';
+            }
         }
-    }
 
-    argComputerMove = getMoveName(randomNumber)
+        argComputerMove = getMoveName(randomNumber)
 
-    printMessage('Mój ruch to ' + argComputerMove)
+        printMessage('Mój ruch to ' + argComputerMove)
 
-    let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.')
+        //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.')
 
-    console.log('Gracz wpisał: ' + playerInput)
+        console.log('Gracz wpisał: ' + playerInput)
 
-    argPlayerMove = getMoveName(playerInput)
+        argPlayerMove = getMoveName(playerInput)
 
-    printMessage('Twój ruch to: ' + argPlayerMove)
+        printMessage('Twój ruch to: ' + argPlayerMove)
 
-    displayResult = function displayResult(argComputerMove, argPlayerMove){
-        printMessage ('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove)
+        displayResult = function displayResult(argComputerMove, argPlayerMove){
+            printMessage ('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove)
+            
+            if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+                return 'Ty wygrywasz!';
+            } 
+            if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+                return 'Ty wygrywasz!';
+            } 
+            if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+                return 'Ty wygrywasz!';
+            } 
+            if (argComputerMove == argPlayerMove){
+                return 'remis';
+            } 
+            if (argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+                return 'Ty przegrywasz';
+            } 
+            if (argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
+                return 'Ty przegrywasz';
+            } 
+            if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+                return 'Ty przegrywasz';
+            } else {
+                return 'Wpisz liczbę od 1 do 3'
+            }
+          
+        } 
         
-        if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-            printMessage('Ty wygrywasz!');
-        } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
-            printMessage('Ty wygrywasz!');
-        } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
-            printMessage('Ty wygrywasz!');
-        } else if (argComputerMove == argPlayerMove){
-            printMessage('remis');
-        } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień'){
-            printMessage('Ty przegrywasz');
-        } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
-            printMessage('Ty przegrywasz');
-        } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
-            printMessage('Ty przegrywasz');
-        } else {
-            printMessage('Wpisz liczbę od 1 do 3');
-        }
-    }
+        printMessage('Wynik to ' + displayResult(argComputerMove, argPlayerMove));
+    }    
 
-    displayResult(argComputerMove, argPlayerMove);
+    document.getElementById('play-rock').addEventListener('click', function () {
+        playGame(1);
+    });
+    document.getElementById('play-paper').addEventListener('click', function () {
+        playGame(2);
+    });
+    document.getElementById('play-scissors').addEventListener('click', function () {
+        playGame(3);
+    });
 }
